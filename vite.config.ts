5af2d395e733051e5af9319f800fb394033a964d
LyudmilaShch <lyudashchepina1999@gmail.com>
2026-06-14 18:56:09 +0300

    fix log

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 import vuetify from 'vite-plugin-vuetify'
 
 export default defineConfig({
-  base: process.env.VITE_BASE_PATH ?? '/',
+  base: '/',
   plugins: [
     vue(),
     vuetify({
